eb8983cf76d5a737d66a4a7b79985e87fec1f092
Bartosiak <bartosiak.contact@gmail.com>
2023-10-18 13:58:47 +0200

    stworzenie pliku

---
+++ js/main .js
@@ -0,0 +1,11 @@
+let number = 200;
+
+function checkNumber(n) {
+    if (100 <= n && n <= 200) {
+        console.log("Liczba znajduje się w przedziale");
+    } else {
+        console.log("Liczba nie znajduje się w przedziale");
+    }
+}
+
+checkNumber(number);
